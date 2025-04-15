e57ceb7dccd200d79edec7df260103e4c1833220
Chris Sheldon <chris.v.sheldon@gmail.com>
2025-04-15 10:30:05 +0100

    Shows windchill instead of temp. Units not returned

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
+import requests, re
 from flask import Flask, jsonify
-import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 URL = "https://scaur.kippfordweather.uk/index.htm"
 
-def get_temperature():
+def get_windchill():
     try:
         response = requests.get(URL, timeout = 10)
         soup = BeautifulSoup(response.content, "html.parser")
@@ -13,14 +13,14 @@ def get_temperature():
         for row in soup.find_all("tr", class_="td_temperature_data"):
             cells = row.find_all("td")
       
-            if cells and "Temperature" in cells[0].text:
-                return cells[1].text.strip()
+            if cells and "Windchill" in cells[0].text:
+                return re.findall("[\d]*[.][\d]+", cells[1].text.strip()
    
     except Exception as e:
         return f"Error: {e}"
 
     return "Unavailable"
       
-@app.route("/kippford/weather/temperature")
+@app.route("/kippford/weather")
 def temperature():
-    return jsonify({"temperature": get_temperature()})
+    return jsonify({"windchill": get_windchill()})
